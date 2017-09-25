d221b2c5de46ad6f68aca71892bdbfb17809514e
Maxime Dor <max@kamax.io>
2017-09-25 17:56:06 +0200

    Fix groovy rollback issue

--- src/main/java/io/kamax/mxisd/MatrixIdentityServerApplication.java
+++ src/main/java/io/kamax/mxisd/MatrixIdentityServerApplication.java
@@ -24,7 +24,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class MatrixIdentityServerApplication {
+public class MatrixIdentityServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MatrixIdentityServerApplication.class, args);
