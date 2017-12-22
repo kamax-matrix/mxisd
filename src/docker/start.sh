6a5a4b3c1c15d24d2096fb0a7cb0ea5c0f441df2
kiorky <kiorky@cryptelium.net>
2017-12-22 02:48:31 +0100

    Fix Docker build (#44)

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
-
-if ! [ -z "$CONF_FILE_PATH" ] && ! [ -f "CONF_FILE_PATH" ]; then
+#!/usr/bin/env bash
+if [[ -n "$CONF_FILE_PATH" ]] && [ ! -f "$CONF_FILE_PATH" ]; then
     echo "Generating config file $CONF_FILE_PATH"
     touch "CONF_FILE_PATH"
 
-    if ! [ -z "$MATRIX_DOMAIN" ]; then
+    if [[ -n "$MATRIX_DOMAIN" ]]; then
         echo "Setting matrix domain to $MATRIX_DOMAIN"
         echo "matrix.domain: $MATRIX_DOMAIN" >> "$CONF_FILE_PATH"
     fi
 
-    if ! [ -z "$SIGN_KEY_PATH" ]; then
+    if [[ -n "$SIGN_KEY_PATH" ]]; then
         echo "Setting signing key path to $SIGN_KEY_PATH"
         echo "key.path: $SIGN_KEY_PATH" >> "$CONF_FILE_PATH"
     fi
 
-    if ! [ -z "$SQLITE_DATABASE_PATH" ]; then
+    if [[ -n "$SQLITE_DATABASE_PATH" ]]; then
         echo "Setting SQLite DB path to $SQLITE_DATABASE_PATH"
         echo "storage.provider.sqlite.database: $SQLITE_DATABASE_PATH" >> "$CONF_FILE_PATH"
     fi
